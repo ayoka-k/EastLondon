cd0f3bfb0b79aeb559e8fd003ead684c0d133d94
Alena Kosheleva <alena@georges-mbp.mynet>
2018-01-29 16:35:04 +0000

    Add four xml files and set content view on each of them.

--- app/src/main/java/com/example/alena/eastlondon/TowerActivity.java
+++ app/src/main/java/com/example/alena/eastlondon/TowerActivity.java
@@ -1,6 +1,13 @@
 package com.example.alena.eastlondon;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class TowerActivity extends AppCompatActivity{
+public class TowerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tower);
+    }
 }
