ea8c7a3bcebc71b0578d630b81bc892acd2f7000
Alena Kosheleva <alena@Georges-MacBook-Pro.local>
2018-02-01 14:21:19 +0000

    Add Fragment to Tower category. Add sight_info xml file with ViewPager and Tabs.

--- app/src/main/java/com/example/alena/eastlondon/TowerActivity.java
+++ app/src/main/java/com/example/alena/eastlondon/TowerActivity.java
@@ -1,7 +1,10 @@
 package com.example.alena.eastlondon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class TowerActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TowerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tower);
+
+        TextView towerOfLondon = findViewById(R.id.tower_of_london_text_view);
+        towerOfLondon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent towerOfLondonIntent = new Intent(TowerActivity.this, TowerOfLondonFragment.class);
+                startActivity(towerOfLondonIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/alena/eastlondon/TowerOfLondonFragment.java
@@ -0,0 +1,27 @@
+package com.example.alena.eastlondon;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TowerOfLondonFragment extends Fragment {
+
+
+    public TowerOfLondonFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
