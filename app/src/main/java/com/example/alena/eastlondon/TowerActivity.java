217ca2f6ee658a6d1e66d3fd342c6d62a03bfc6e
Alena Kosheleva <alena@Georges-MacBook-Pro.local>
2018-02-01 15:12:25 +0000

    Add SightActivity to display each sight.

--- app/src/main/java/com/example/alena/eastlondon/TowerActivity.java
+++ app/src/main/java/com/example/alena/eastlondon/TowerActivity.java
@@ -13,12 +13,16 @@ public class TowerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tower);
 
+        //Array to keep names of sights for Tower category
+        final String [] towerSights = new String[3];
+        towerSights[0] = "Tower of London";
+        towerSights[1] = "Tower Bridge";
+        towerSights[2] = "Wilton'\'s Music Hall";
+
         TextView towerOfLondon = findViewById(R.id.tower_of_london_text_view);
         towerOfLondon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent towerOfLondonIntent = new Intent(TowerActivity.this, TowerOfLondonFragment.class);
-                startActivity(towerOfLondonIntent);
             }
         });
     }
